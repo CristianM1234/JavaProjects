fd4bb6e739136b01700de93232806f9d3d6db5e2
CristianM1234 <143001363+CristianM1234@users.noreply.github.com>
2023-08-23 08:45:38 -0600

    Add files via upload

---
+++ PrimerosProgramas/Tree.java
@@ -0,0 +1,22 @@
+
+package tree;
+
+
+public class Tree {
+
+    public static void main(String[] args) {
+     System.out.println("Hey there, i'm Cristian and i'm learning to code in java! ");
+     System.out.println("i'm ready to plant a tree today!" + "Ready to get my7 hands dirty!" );
+     System.out.println("   *   ");
+     System.out.println("  ***  ");
+     System.out.println("*******");
+     System.out.println(" ***** ");
+     System.out.println("  ***  ");
+     System.out.println("   *   ");
+     System.out.println("   *   ");
+     System.out.println("   *   ");
+     System.out.println("   *  ");
+     
+    }
+    
+}
