7906704449e4cfa6b1dbbd79e57dc4422b6228ad
CristianM1234 <143001363+CristianM1234@users.noreply.github.com>
2023-09-06 09:55:53 -0500

    Add files via upload

---
+++ PrimerosProgramas/EqualNotEqual.java
@@ -0,0 +1,16 @@
+package equalnotequal;
+
+public class EqualNotEqual {
+
+    public static void main(String[] args) {
+        int songsA = 9;
+    int songsB = 9;
+    int albumLengthA = 41;
+    int albumLengthB = 53;
+    boolean sameNumberOfSongs = songsA == songsB;
+    boolean differentLength = songsA != songsB;
+    System.out.println(sameNumberOfSongs);
+    System.out.println(differentLength);
+    }
+    
+}
