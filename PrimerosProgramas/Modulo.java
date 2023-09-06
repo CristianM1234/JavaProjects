99ded3a393542c23f46c66bb6f1164371046889f
CristianM1234 <143001363+CristianM1234@users.noreply.github.com>
2023-09-06 09:27:21 -0500

    Add files via upload

---
+++ PrimerosProgramas/Modulo.java
@@ -0,0 +1,11 @@
+package modulo;
+
+public class Modulo {
+
+    public static void main(String[] args) {
+    int students = 26;
+    int leftOut = students%3;
+    System.out.println(leftOut);
+    }
+    
+}
