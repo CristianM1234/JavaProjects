23d7b7e5cc1564efa71384b9bf9d4a5bfabb531c
CristianM1234 <143001363+CristianM1234@users.noreply.github.com>
2023-09-06 09:21:42 -0500

    Add files via upload

---
+++ PrimerosProgramas/Final.java
@@ -0,0 +1,12 @@
+package pkgfinal;
+
+public class Final {
+public static void main(String[] args) {
+    final double pi = 3.14;
+    System.out.println(pi);
+    //pi = 3.14*2; no se puede, ya que no se le puede dar otro valor a pi dentro de la función
+   
+    
+    }
+    
+}
