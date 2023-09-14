926556ebc8ed055b0141224487bb04e47c23eb52
CristianM1234 <143001363+CristianM1234@users.noreply.github.com>
2023-09-14 09:50:55 -0500

    Add files via upload

---
+++ AccessEncapsulationScope/ThisKey/SavingsAccount.java
@@ -0,0 +1,23 @@
+package savingsaccount;
+
+public class SavingsAccount {
+public String owner;
+public int balanceDollar;
+public double balanceEuro;
+
+public SavingsAccount(String owner, int balanceDollar){
+    owner = owner;
+    balanceDollar = balanceDollar;
+    balanceEuro = balanceDollar*0.85;
+}
+public void addMoney(int balanceDollar){
+    System.out.println("Addimg " + balanceDollar + " dollars to the account.");
+    this.balanceDollar += balanceDollar;
+    System.out.print("he new balance is " + this.balanceDollar + " dollars.");
+}
+    public static void main(String[] args) {
+        SavingsAccount zeusSavingsAccount = new SavingsAccount("Zeus", 1000);
+        zeusSavingsAccount.addMoney(2000);
+    }
+    
+}
