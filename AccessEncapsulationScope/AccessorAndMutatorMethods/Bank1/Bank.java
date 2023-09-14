75fe09dbd0fc4e8277653c14f91d729e3e32a7a6
CristianM1234 <143001363+CristianM1234@users.noreply.github.com>
2023-09-14 09:27:50 -0500

    Update Bank.java

--- AccessEncapsulationScope/AccessorAndMutatorMethods/Bank1/Bank.java
+++ AccessEncapsulationScope/AccessorAndMutatorMethods/Bank1/Bank.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package bank;
+public class Bank{
+  private CheckingAccount accountOne;
+  private CheckingAccount accountTwo;
 
-/**
- *
- * @author Estudiantes
- */
-public class Bank {
+  public Bank(){
+    accountOne = new CheckingAccount("Zeus", 100, "1");
+    accountTwo = new CheckingAccount("Hades", 200, "2");
+  }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+  public static void main(String[] args){
+    Bank bankOfGods = new Bank();
+    System.out.println(bankOfGods.accountOne.name);
+    bankOfGods.accountOne.addFunds(5);
+    bankOfGods.accountOne.getInfo();
+
+  }
+
 }
